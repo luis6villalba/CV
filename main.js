23a9f8490f2e73f8c3eb9fc7bbe3454f873b7bec
Luis A. Villalba <luisvillalba_12@hotmail.com>
2024-06-25 22:17:51 -0500

    Se realizaron ajustes y se probo el funcionamiento del archivo main.js

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 let listElements = document.querySelectorAll('.list__button--click');
 
 listElements.forEach(listElement => {
-    listElement.addEventListener('click', () =>{
+    listElement.addEventListener('click', ()=>{
 
         listElement.classList.toggle('arrow');
 
         let height = 0;
         let menu = listElement.nextElementSibling;
-        if(menu.clientHeight == ("0"){
+        if(menu.clientHeight == "0"){
             height=menu.scrollHeight;
         }
 
