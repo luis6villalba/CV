13de7f117fd76543d787fa5162c83c42916fbac4
Luis A. Villalba <luisvillalba_12@hotmail.com>
2024-07-01 19:12:37 -0500

    ajustando archivo app.js, se crean las funciones

---
+++ js/app.js
@@ -0,0 +1,55 @@
+(function(){
+    const listElements = document.querySelectorAll('.navbar__item--show');
+    const list = document.querySelector('.navbar__links');
+    const navbar = document.querySelector('.navbar__hamburger');
+    
+    const addClick = ()=>{
+        listElements.forEach(element =>{
+            element.addEventListener('click', ()=>{
+
+                let subMenu = element.children[1];
+                let height = 0;
+                element.classList.toggle('navbar__item--active');
+
+                /*console.log(subMenu.clientHeight);*/
+
+                if(subMenu.clientHeight === 0){
+                    height = subMenu.scrollHeight;
+                }
+
+                subMenu.style.height = `${height}px`;
+
+            });
+
+        });
+    }
+
+    const deleteStyleHeight = ()=>{
+        listElements.forEach(element=>{
+            if(element.children[1].getAttribute('style')){
+                element.children[1].removeAttribute('style');
+                element.classList.remove('navbar__item--active');
+            }
+        });
+    }
+
+    /*addClick();*/
+
+    window.addEventListener('resize', ()=>{
+        if(window.innerWidth > 678){
+            deleteStyleHeight();
+            if(list.classList.contains('navbar__links--show'))
+                list.classList.remove('menu__links--show');
+
+        }else{
+            addClick();
+        }
+    });
+
+    if(window.innerWidth <= 678){
+        addClick();
+    }
+
+    navbar.addEventListener('click', ()=> list.classList.toggle('navbar__links--show'));
+
+})();
